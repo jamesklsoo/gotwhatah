0429c7c8340f3e593eb90524bd0329052cd1c4aa
alvintks <alvintankuansean@gmail.com>
2017-07-24 15:06:37 +0800

    fixed clearance gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+  include Clearance::User
   validates :email, uniqueness: true, presence: true
   validates :username, uniqueness: true, presence: true
   validates :password, presence: true
-  validates :name, presence: true
 
   has_many :events
   has_many :comments
@@ -11,13 +11,4 @@ class User < ApplicationRecord
   belongs_to :location
 
   enum status: [:newbie, :intermediate, :expert]
-
-  def check_status 
-    if @user.comments.count == 0 && <10 
-      @user.update!(status: 0)
-    elsif @user.comments.count == >9 && <20 
-      @user.update!(status: 1)
-    else 
-      @user.update!(status: 2) 
-  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   resources :comments
   resources :events
   resources :locations
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
+
 end
