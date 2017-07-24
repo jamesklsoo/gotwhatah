2f232c55e06918edd5c9f2a97bf4f340b81a294b
James Soo <jamesklsoo@gmail.com>
2017-07-24 10:53:09 +0800

    add validates and associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  belongs_to :user
+  belongs_to :location
+  enum category:[:food, :lifestyle, :entertainment]
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+  has_many :users
+  enum area: [:subang_jaya, :damansara, :ttdi]
 end
