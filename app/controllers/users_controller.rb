6e8c83633ce296b4f02ae74448ed8142e02ba09f
James Soo <jamesklsoo@gmail.com>
2017-07-25 10:43:59 +0800

    access individual event page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < Clearance::UsersController
 
-  def create 
+  def create
     @user = User.new(user_params)
-      if @user.save
-        sign_in @user
-        redirect_to user_events_path
-      else
-        @error = @user.errors.full_messages
-        respond_to do |format|
+    if @user.save
+      sign_in @user
+      redirect_to events_path
+    else
+      @error = @user.errors.full_messages
+      respond_to do |format|
         format.js
         format.html { render template: "users/new" }
-        end
       end
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
-  end 
+  end
 end
