92992de6d53e312fa1ed5d00d8f261badd23fd72
James Soo <jamesklsoo@gmail.com>
2017-07-26 18:19:04 +0800

    jquery for voting

--- app/assets/javascripts/events.js
+++
@@ -1,5 +0,0 @@
-// $(document).ready(function() {
-//   $(".vote").click(function(event){
-//     event.preventDefault();
-//   });
-// });
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
   validates :description, presence: true
   belongs_to :user, optional: true
   has_many :votes, dependent: :destroy
+  has_many :comments
   enum area: [:subang_jaya, :damansara, :ttdi]
   enum category:[:food, :lifestyle, :entertainment]
   acts_as_votable
