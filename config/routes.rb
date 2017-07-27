82fe81910ab630530e977c17340106af81149ce3
James Soo <jamesklsoo@gmail.com>
2017-07-27 15:16:35 +0800

    update welcome index and routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
     resources :comments, controller: "comments"
   end
 
+  resources :events, controller: "events" do
+    resources :comments, controller: "comments", only: [:create, :new, :update, :destroy]
+  end
+
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
