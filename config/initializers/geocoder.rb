41f8f09409ffea956567c8f44b15ed8d4b685e92
James Soo <jamesklsoo@gmail.com>
2017-07-30 21:24:27 +0800

    update quotaguard

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,7 +3,7 @@ Geocoder.configure(
   # set default units to kilometers:
   :units => :km,
 
-  :http_proxy => ENV['http://q1qe0wlup3pzr7:wP14OLnF_nHgWqCTZxAztqcckQ@proxy.quotaguard.com:9292'],
+  :http_proxy => ENV['QUOTAGUARD_URL'],
   :timeout => 5
 
 )
