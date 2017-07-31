3d905d60e9d7b2eb09d8909c7b349ad8f53b0d67
James Soo <jamesklsoo@gmail.com>
2017-07-31 11:05:42 +0800

    update user status

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,21 @@ class User < ApplicationRecord
   has_many :comments
   has_many :votes
   acts_as_voter
-  enum status: [:newbie, :intermediate, :expert]
+  enum status: [:kid, :awesome, :legend]
 
   geocoded_by :address,:latitude => :latitude, :longitude => :longitude
   after_validation :geocode
 
   def check_status(user)
     if Event.where(user_id: user.id).count == 0 || Event.where(user_id: user.id).count < 5
-      user.newbie!
-    elsif Event.where(user_id: user.id).count > 4 || Event.where(user_id: user.id).count < 10
-      user.intermediate!
+      user.kid!
+      user.status
+    elsif Event.where(user_id: user.id).count > 4 || Event.where(user_id: user.id).count < 7
+      user.awesome!
+      user.status
     else
-      user.expert!
+      user.legend!
+      user.status
     end
   end
 end
